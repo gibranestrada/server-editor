33ce7cd714c1656eb938564775b6b0db418670c8
Gibran Estrada <gibran9@gmail.com>
2020-02-24 16:52:29 -0500

    changed https to http

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const uniqid = require("uniqid");
 const webSocketsServerPort = process.env.PORT || 3000;
 const webSocketServer = require("websocket").server;
-const http = require("https");
+const http = require("http");
 // Spinning the http server and the websocket server.
 const server = http.createServer();
 server.listen(webSocketsServerPort);
